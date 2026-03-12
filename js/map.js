feb632ebaa38f75b7c7bf08fcb8df9c5e981ab83
mkokoba <mkokoba@gmail.com>
2026-03-12 17:15:44 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -106,61 +106,26 @@ fetch('map/wereda1.json')   // change to .geojson if your file uses that extensi
     console.error("Error loading Wereda layer:", error);
   });
 
-// === Road Layer ===
 
 // === Road Layer ===
+loadGeoJSON('map/road1.geojson', {
+  style: function (feature) {
+    const colors = {
+      trunk: '#e41a1c',
+      primary: '#377eb8',
+      secondary: '#4daf4a',
+      tertiary: '#984ea3',
+      residential: '#ff7f00'
+    };
 
-fetch('map/road1.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Road file not found");
-    }
-    return response.json();
-  })
-  .then(data => {
-
-    console.log("Road data loaded:", data);
-
-    roadLayer = L.geoJSON(data, {
-
-      style: function (feature) {
-
-        const roadType = feature.properties.fclass || 
-                         feature.properties.type || 
-                         "other";
-
-        const colors = {
-          trunk: "#e41a1c",
-          primary: "#377eb8",
-          secondary: "#4daf4a",
-          tertiary: "#984ea3",
-          residential: "#ff7f00",
-          other: "#999"
-        };
-
-        return {
-          color: colors[roadType] || "#999",
-          weight: 3
-        };
-      },
-
-      onEachFeature: function (feature, layer) {
-
-        const roadName = feature.properties.name || "Road";
-        const roadType = feature.properties.fclass || "Unknown";
-
-        layer.bindPopup(
-          "<b>Road:</b> " + roadName +
-          "<br><b>Type:</b> " + roadType
-        );
-      }
-
-    }).addTo(map);
-
-  })
-  .catch(error => {
-    console.error("Error loading road layer:", error);
-  });
+    return {
+      color: colors[feature.properties.fclass] || '#999',
+      weight: 2
+    };
+  }
+}).then(layer => {
+  roadLayer = layer;
+});
 
 // === Tourist Site Layer ===
 loadGeoJSON('map/siteF.geojson', {
@@ -389,6 +354,7 @@ function showSiteInfo(props) {
   `;
   info.style.display = 'block';
 }
+
 
 
 
